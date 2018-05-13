862bdb7192717e466d519dad1f75d4158101c3c6
Anastasiia <nastena31011993@gmail.com>
2018-05-13 23:25:54 +0300

    Added parking service and controller

---
+++ CarParkingApi/Controllers/ParkingController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarParkingApi.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarParkingApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Parking")]
+    public class ParkingController : Controller
+    {
+        private readonly ParkingService parkingService;
+
+        public ParkingController(ParkingService parkingService)
+        {
+            this.parkingService = parkingService;
+        }
+
+        [HttpGet("FreeSpaces")]
+        public IActionResult GetFreeSpaces()
+        {
+            try
+            {
+                return Json(parkingService.GetFreeSpaces());
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+        }
+
+        [HttpGet("TakenSpaces")]
+        public IActionResult GetTakenSpaces()
+        {
+            try
+            {
+                return Json(parkingService.GetTakenSpaces());
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+        }
+    }
+}
---
+++ CarParkingApi/Service/ParkingService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarParking.Entities;
+
+namespace CarParkingApi.Service
+{
+    public class ParkingService
+    {
+        private readonly Parking parking;
+
+        public ParkingService()
+        {
+            parking = Parking.Instance;
+        }
+
+        public int GetFreeSpaces()
+        {
+            return parking.GetFreeSpacesNumber();
+        }
+
+        public int GetTakenSpaces()
+        {
+            return parking.GetTakenSpacesNumber();
+        }
+    }
+}
